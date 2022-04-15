0877ff9d2c2dcf4e7c79d6ce6a290d5cee962c38
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2022-04-15 16:15:20 +0100

    [Feature] adding Unittest for FileStorage and DBStorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -76,23 +76,28 @@ class DBStorage:
         self.__session.remove()
 
     def get(self, cls, id):
+        """ return the specified object by id
         """
-        Returns the object based on the class name and its ID, or None if not
-        found
-        """
-        objects = self.__session.query(classes[cls])
-        for obj in objects:
-            if obj.id == id:
-                return obj
-        return None
+        if cls in classes.keys():
+            objs = self.__session.query(eval(cls))
+            for elem in objs:
+                if (elem.id == id):
+                    return (elem)
+        return (None)
 
     def count(self, cls=None):
+        """ return number of specified class
         """
-        Returns the number of objects in storage matching the given class name.
-        If no name is passed, returns the count of all objects in storage.
-        """
-        nobjects = 0
-        for clss in classes:
-            if cls is None or cls is classes[clss] or cls is clss:
-                nobjects += len(self.__session.query(classes[clss]).all())
-        return nobjects
+        count = 0
+        if cls is None:
+            for clss in classes:
+                objs = self.__session.query(classes[clss]).all()
+                for obj in objs:
+                    count = count + 1
+            return (count)
+        else:
+            if cls in classes.keys():
+                objs = self.__session.query(eval(cls))
+                for obj in objs:
+                    count = count + 1
+            return (count)
